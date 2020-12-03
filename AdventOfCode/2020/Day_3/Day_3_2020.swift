87a1b7f26e0ba5470c2810a8d22f935153e7f2fa
Marcus Gollnick <mgo@adorsys.de>
2020-12-03 20:14:25 +0100

    Solve Day 3 in 2020

--- AdventOfCode/2020/Day_3/Day_3_2020.swift
+++ AdventOfCode/2020/Day_3/Day_3_2020.swift
@@ -20,10 +20,52 @@ enum Day_3_2020: Solvable {
     }
 
     private static func solvePart1(lines: [String]) -> String {
-        "Add some code here"
+        let map =  reuseTreePattern(map: lines, multiplier: 3)
+        let positions = getPositions(stepsRight: 3, stepsDown: 1, height: map.count)
+        var trees = 0
+        for pos in positions {
+            let row = Array(map[pos.y])
+            if row[pos.x] == "#" {
+                trees += 1
+            }
+        }
+        return "\(trees)"
     }
 
     private static func solvePart2(lines: [String]) -> String {
-        "Add some code here"
+        let moves:[(right: Int, down: Int)] = [(1,1),(3,1),(5,1),(7,1),(1,2)]
+        var trees: [Int] = []
+        for move in moves {
+            let map =  reuseTreePattern(map: lines, multiplier: move.right)
+            let positions = getPositions(stepsRight: move.right, stepsDown: move.down, height: map.count)
+            var tree = 0
+            for pos in positions {
+                let row = Array(map[pos.y])
+                if row[pos.x] == "#" {
+                    tree += 1
+                }
+            }
+            trees.append(tree)
+        }
+
+        return "\(trees.reduce(1, *))"
+    }
+
+    private static func reuseTreePattern(map: [String], multiplier: Int) -> [String] {
+        var newMap = map
+        let minWidth = map.count * multiplier
+        while newMap[0].count < minWidth {
+            newMap = newMap.map{ $0 + $0 }
+        }
+        return newMap
+    }
+
+    private static func getPositions(stepsRight: Int, stepsDown: Int, height: Int) -> [(x:Int, y:Int)] {
+        var positions: [(x:Int, y:Int)] = []
+        let setps = height / stepsDown
+        for i in 1..<setps {
+            positions.append((stepsRight*i, stepsDown*i))
+        }
+        return positions
     }
 }
