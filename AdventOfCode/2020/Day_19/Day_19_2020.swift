c2a29f06a28f03cbac06abb0795f560242553971
Marcus Gollnick <mgo@adorsys.de>
2020-12-19 12:11:10 +0100

    Solve Day 19 in 2020

--- AdventOfCode/2020/Day_19/Day_19_2020.swift
+++ AdventOfCode/2020/Day_19/Day_19_2020.swift
@@ -37,7 +37,7 @@ enum Day_19_2020: Solvable {
                 guard ruleLine.count == 2,
                       let number = Int(ruleLine.first ?? "")
                 else {
-                    fatalError("Can not form Int Nubmer in Rule")
+                    fatalError("Can not form Int Number in Rule")
                 }
 
                 if ruleLine[1].contains("|") {
@@ -51,12 +51,111 @@ enum Day_19_2020: Solvable {
             }
         }
 
-        guard let ruleZero = rules.first(where: { $0.number == 0 }) else {
+        var zeroRulesSet: Set<String> = []
+
+        for rule in getZeroRules(for: 0, in: rules) {
+            zeroRulesSet.insert(rule.replacingOccurrences(of: " ", with: ""))
+        }
+
+        var validMessages = 0
+        for message in messages {
+            if zeroRulesSet.contains(message) {
+                validMessages += 1
+            }
+        }
+
+        return "\(validMessages)"
+    }
+
+    static func solvePart2(input: [String]) -> String {
+        var rules: [Rule] = []
+        var messages: [String] = []
+        var isRules = true
+        for var line in input {
+            if line == "" {
+                isRules = false
+                continue
+            } else if line.dropLast(line.count-2) == "8:" {
+                line = "8: 42 | 42 8"
+            } else if line.dropLast(line.count-3) == "11:" {
+                line = "11: 42 31 | 42 11 31"
+            }
+            if isRules {
+                let ruleLine = line
+                    .replacingOccurrences(of: "\"", with: "")
+                    .components(separatedBy: ": ")
+                guard ruleLine.count == 2,
+                      let number = Int(ruleLine.first ?? "")
+                else {
+                    fatalError("Can not form Int Number in Rule")
+                }
+
+                if ruleLine[1].contains("|") {
+                    let cmp = ruleLine[1].components(separatedBy: " | ")
+                    rules.append(.init(number: number, value1: cmp[0], value2: cmp[1]))
+                } else {
+                    rules.append(.init(number: number, value1: ruleLine[1], value2: ""))
+                }
+            } else {
+                messages.append(line)
+            }
+        }
+
+        let messageSizes = messages.map { $0.count }
+
+        let fourtyTwo = getZeroRules(for: 42, in: rules).map{ $0.replacingOccurrences(of: " ", with: "")}
+        let thirtyOne = getZeroRules(for: 31, in: rules).map{ $0.replacingOccurrences(of: " ", with: "")}
+
+        let size = (messageSizes.max() ?? 0) / ((fourtyTwo.first ?? "").replacingOccurrences(of: " ", with: "").count)
+        let zeroes = getZeroRules(for: 0, in: rules, breakAfterIteration: true, size: size*2)
+
+        var messageSet: Set<String> = []
+        messages.forEach { messageSet.insert($0) }
+
+
+        var splitedMessages: [[String]] = []
+        for message in messageSet {
+            splitedMessages.append(message.split(every: ((fourtyTwo.first ?? "").replacingOccurrences(of: " ", with: "").count)))
+        }
+
+        var newSplitedMessages: [[String]] = []
+        for message in splitedMessages {
+            var resultingMessage: [String] = []
+            for split in message {
+                if fourtyTwo.contains(split) {
+                    resultingMessage.append("42")
+                } else if thirtyOne.contains(split) {
+                    resultingMessage.append("31")
+                } else {
+                    resultingMessage.append("nan")
+                }
+            }
+            newSplitedMessages.append(resultingMessage)
+        }
+
+        let newMessages = newSplitedMessages.map { $0.joined(separator: " ")}
+        var validMessages = 0
+
+        for message in newMessages {
+            if zeroes.contains(message) {
+                validMessages += 1
+            }
+        }
+
+        return "\(validMessages)"
+    }
+
+    static func getZeroRules(for zero: Int, in rules: [Rule], breakAfterIteration: Bool = false, size: Int = 0) -> Set<String> {
+
+        guard let ruleZero = rules.first(where: { $0.number == zero }) else {
             fatalError("Can not find rule zero")
         }
 
         var zeroRules: Set<String> = [ruleZero.value1]
-        var zeroRulesSet: Set<String> = []
+
+        if ruleZero.value2 != "" {
+            zeroRules.insert(ruleZero.value2)
+        }
 
         var run = true
         var values: [String] = []
@@ -71,6 +170,10 @@ enum Day_19_2020: Solvable {
                         guard let number = Int(value),
                               let newRule = rules.first(where: { $0.number == number })
                         else { continue }
+                        
+                        if breakAfterIteration && number != 8 && number != 11 {
+                            continue
+                        }
 
                         if newRule.value2 == "" {
                             for (index, var rule) in newRules.enumerated() {
@@ -102,43 +205,31 @@ enum Day_19_2020: Solvable {
             for rule in zeroRules {
                 if rule.trimmingCharacters(in: Day_19_2020.allowedCharacters).isEmpty {
                     good += 1
-//                    zeroRulesSet.insert(rule.replacingOccurrences(of: " ", with: ""))
-//                    zeroRules.remove(rule)
                 }
             }
-            print("\(good) / \(zeroRules.count)")
+
             if good == zeroRules.count {
                 run = false
             }
-        }
 
-        for rule in zeroRules {
-            zeroRulesSet.insert(rule.replacingOccurrences(of: " ", with: ""))
-        }
-
-        var validMessages = 0
-        for message in messages {
-            if zeroRulesSet.contains(message) {
-                validMessages += 1
+            if breakAfterIteration {
+                for rule in zeroRules {
+                    if rule.components(separatedBy: " ").count >= size {
+                        run = false
+                        break
+                    }
+                }
             }
         }
 
-        return "\(validMessages)"
-    }
-
-    static func solvePart2(input: [String]) -> String {
-        "Add some Code here"
+        return zeroRules
     }
 }
 
-/*
- 8 11
- 42 42 31
+extension String {
+    func split(every length:Int) -> [String] {
+        guard length > 0 && length < count else { return [String(suffix(from:startIndex))] }
 
- 120 16 120 16 31
- 120 16 2 126 31
- 2 126 120 16 31
- 2 126 2 126 31
-
-
- */
+        return (0 ... (count - 1) / length).map { String(dropFirst($0 * length).prefix(length)) }
+    }
+}
