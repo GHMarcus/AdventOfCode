279bdcce18a462a3c5139d7e05a81d44a60ba8f0
Marcus Gollnick <marcus.gollnick@datev.de>
2021-11-19 10:42:18 +0100

    Solve Day 10 in 2015

--- AdventOfCode/2015/Day_10/Day_10_2015.swift
+++ AdventOfCode/2015/Day_10/Day_10_2015.swift
@@ -11,11 +11,81 @@ enum Day_10_2015: Solvable {
     static var day: Input.Day = .Day_10
     static var year: Input.Year = .Year_2015
 
-    static func solvePart1(input: [Int]) -> String {
-        "Add some Code here"
+    static func solvePart1(input: [String]) -> String {
+        let rounds = 40
+        var digits = Array(input[0])
+        for i in 1...rounds {
+            
+            var newDigits: [(amount: Int, number: String.Element)] = []
+            var previousDigit: String.Element? = nil
+            var digitAmount: Int = 0
+            
+            print("Round \(i)")
+            
+            for n in 0..<digits.count {
+                let digit = digits[n]
+                if previousDigit == nil {
+                    previousDigit = digit
+                    digitAmount += 1
+                } else if previousDigit == digit {
+                    digitAmount += 1
+                } else {
+                    newDigits.append((digitAmount,previousDigit!))
+                    previousDigit = digit
+                    digitAmount = 1
+                }
+                if n == digits.count - 1 {
+                    newDigits.append((digitAmount,previousDigit!))
+                }
+            }
+            
+            digits = []
+            for newDigit in newDigits {
+                digits.append(String(newDigit.amount).first!)
+                digits.append(newDigit.number)
+            }
+        }
+        print("End")
+        
+        return "\(digits.count)"
     }
 
-    static func solvePart2(input: [Int]) -> String {
-        "Add some Code here"
+    static func solvePart2(input: [String]) -> String {
+        let rounds = 50
+        var digits = Array(input[0])
+        for i in 1...rounds {
+            
+            var newDigits: [(amount: Int, number: String.Element)] = []
+            var previousDigit: String.Element? = nil
+            var digitAmount: Int = 0
+            
+            print("Round \(i)")
+            
+            for n in 0..<digits.count {
+                let digit = digits[n]
+                if previousDigit == nil {
+                    previousDigit = digit
+                    digitAmount += 1
+                } else if previousDigit == digit {
+                    digitAmount += 1
+                } else {
+                    newDigits.append((digitAmount,previousDigit!))
+                    previousDigit = digit
+                    digitAmount = 1
+                }
+                if n == digits.count - 1 {
+                    newDigits.append((digitAmount,previousDigit!))
+                }
+            }
+            
+            digits = []
+            for newDigit in newDigits {
+                digits.append(String(newDigit.amount).first!)
+                digits.append(newDigit.number)
+            }
+        }
+        print("End")
+        
+        return "\(digits.count)"
     }
 }
