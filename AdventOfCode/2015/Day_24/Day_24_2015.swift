5d1ba65c8688f7e63f5fbf9bce3916c78214de69
Marcus Gollnick <marcus.gollnick@datev.de>
2021-11-24 16:23:56 +0100

    Solve Day 24 in 2015

--- AdventOfCode/2015/Day_24/Day_24_2015.swift
+++ AdventOfCode/2015/Day_24/Day_24_2015.swift
@@ -12,10 +12,28 @@ enum Day_24_2015: Solvable {
     static var year: Input.Year = .Year_2015
 
     static func solvePart1(input: [Int]) -> String {
-        "Add some Code here"
+        let sum = input.reduce(0, +)
+        let combinations = perfectSumCombinations(for: input, sum: sum/3)
+        
+        let amountOfPackages = combinations.map { $0.count }
+        let minimumAmount = amountOfPackages.min() ?? 0
+        let possibleFirstGroupCombinations = combinations.filter { $0.count == minimumAmount }
+        
+        let quantumEntanglementsForPossibleCombinations = possibleFirstGroupCombinations.map { $0.reduce(1, *) }
+        
+        return "\(quantumEntanglementsForPossibleCombinations.min() ?? 0)"
     }
 
     static func solvePart2(input: [Int]) -> String {
-        "Add some Code here"
+        let sum = input.reduce(0, +)
+        let combinations = perfectSumCombinations(for: input, sum: sum/4)
+        
+        let amountOfPackages = combinations.map { $0.count }
+        let minimumAmount = amountOfPackages.min() ?? 0
+        let possibleFirstGroupCombinations = combinations.filter { $0.count == minimumAmount }
+        
+        let quantumEntanglementsForPossibleCombinations = possibleFirstGroupCombinations.map { $0.reduce(1, *) }
+        
+        return "\(quantumEntanglementsForPossibleCombinations.min() ?? 0)"
     }
 }
