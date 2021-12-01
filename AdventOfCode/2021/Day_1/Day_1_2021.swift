e205272d780a05b9ea104d3be383e23f31bf24d9
Marcus Gollnick <marcus.gollnick@datev.de>
2021-12-01 06:54:31 +0100

    Solve Day 1 in 2021

--- AdventOfCode/2021/Day_1/Day_1_2021.swift
+++ AdventOfCode/2021/Day_1/Day_1_2021.swift
@@ -11,11 +11,28 @@ enum Day_1_2021: Solvable {
     static var day: Input.Day = .Day_1
     static var year: Input.Year = .Year_2021
 
-    static func solvePart1(input: [String]) -> String {
-        "Add some Code here"
+    static func solvePart1(input: [Int]) -> String {
+        var previousValue = Int.max
+        var increasings = 0
+        for measurement in input {
+            if measurement > previousValue {
+                increasings += 1
+            }
+            previousValue = measurement
+        }
+        return "\(increasings)"
     }
 
-    static func solvePart2(input: [String]) -> String {
-        return "Add some Code here"
+    static func solvePart2(input: [Int]) -> String {
+        var previousValue = Int.max
+        var increasings = 0
+        for i in 0...input.count-3 {
+            let window = input[i] + input[i+1] + input[i+2]
+            if window > previousValue {
+                increasings += 1
+            }
+            previousValue = window
+        }
+        return "\(increasings)"
     }
 }
