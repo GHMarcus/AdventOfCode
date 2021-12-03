dee4a3af7b435220e5c0885b8754521e1ac1c508
Marcus Gollnick <mgo@adorsys.de>
2021-12-03 12:40:05 +0100

    Solve Day 24 and 25 in 2020

--- AdventOfCode/2020/Day_24/Day_24_2020.swift
+++ AdventOfCode/2020/Day_24/Day_24_2020.swift
@@ -7,15 +7,139 @@
 
 // https://adventofcode.com/2020/day/24
 
+import Foundation
+
 enum Day_24_2020: Solvable {
     static var day: Input.Day = .Day_24
     static var year: Input.Year = .Year_2020
 
-    static func solvePart1(input: [Int]) -> String {
-        "Add some Code here"
+    struct Pos: Hashable {
+        let x: Decimal
+        let y: Decimal
     }
 
-    static func solvePart2(input: [Int]) -> String {
-        "Add some Code here"
+    static var tiles: Dictionary<Pos, Bool> = [:]
+    static var newTiles: Dictionary<Pos, Bool> = [:]
+
+    static func solvePart1(input: [String]) -> String {
+        let start = Pos(x: 0.0,y: 0.0)
+        tiles[start] = false
+        var current = start
+        for line in input {
+            current = start
+            var directions = Array(line)
+//            var dStr = ""
+            while !directions.isEmpty {
+                var direction = String(directions.removeFirst())
+                if direction == "s" || direction == "n" {
+                    direction += String(directions.removeFirst())
+                }
+//                dStr += " " + direction
+                switch direction {
+                case "e":
+                    current = Pos(x: current.x+1.0, y: current.y)
+                case "w":
+                    current = Pos(x: current.x-1.0, y: current.y)
+                case "se":
+                    let yShift = current.y + 1.0
+                    let xShift = current.x + 0.5
+                    current = Pos(x: xShift, y: yShift)
+                case "sw":
+                    let yShift = current.y + 1.0
+                    let xShift = current.x - 0.5
+                    current = Pos(x: xShift, y: yShift)
+                case "ne":
+                    let yShift = current.y - 1.0
+                    let xShift = current.x + 0.5
+                    current = Pos(x: xShift, y: yShift)
+                case "nw":
+                    let yShift = current.y - 1.0
+                    let xShift = current.x - 0.5
+                    current = Pos(x: xShift, y: yShift)
+                default:
+                    fatalError("Should never be executed")
+                }
+            }
+//            print("\(current): \(dStr)")
+            if var tile = tiles[current] {
+                tile.toggle()
+                tiles[current] = tile
+            } else {
+                tiles[current] = true
+            }
+        }
+//        print(tiles.count)
+//        print(tiles.filter({ $0.value }).count)
+        return "\(tiles.filter({ $0.value }).count)"
+    }
+
+    static func solvePart2(input: [String]) -> String {
+        var day = 0
+        let maxDay = 100
+
+        while day < maxDay {
+            newTiles = [:]
+
+            for tile in tiles {
+                addAllNewNeighbours(for: tile.key)
+            }
+
+            for tile in tiles {
+                let neighbours = getNeighbours(for: tile.key).filter{$0}
+                if tile.value && neighbours.count == 0 {
+                    newTiles[tile.key] = false
+                } else if tile.value && neighbours.count > 2 {
+                    newTiles[tile.key] = false
+                }else if !tile.value && neighbours.count == 2 {
+                    newTiles[tile.key] = true
+                } else {
+                    newTiles[tile.key] = tile.value
+                }
+            }
+            tiles = newTiles
+            day += 1
+            //print("Day \(day): \(tiles.filter({ $0.value }).count)")
+        }
+
+        return "\(tiles.filter({ $0.value }).count)"
+    }
+
+    static func getNeighbours(for tilePos: Pos) -> [Bool] {
+        var neighbours: [Bool] = []
+        let positions = [
+            Pos(x: tilePos.x-1, y: tilePos.y),
+            Pos(x: tilePos.x-0.5, y: tilePos.y-1),
+            Pos(x: tilePos.x+0.5, y: tilePos.y-1),
+            Pos(x: tilePos.x+1, y: tilePos.y),
+            Pos(x: tilePos.x+0.5, y: tilePos.y+1),
+            Pos(x: tilePos.x-0.5, y: tilePos.y+1)
+        ]
+
+        for pos in positions {
+            if let value = tiles[pos] {
+                neighbours.append(value)
+            } else {
+                neighbours.append(false)
+            }
+        }
+
+        return neighbours
+    }
+
+    static func addAllNewNeighbours(for tilePos: Pos) {
+        let positions = [
+            Pos(x: tilePos.x-1, y: tilePos.y),
+            Pos(x: tilePos.x-0.5, y: tilePos.y-1),
+            Pos(x: tilePos.x+0.5, y: tilePos.y-1),
+            Pos(x: tilePos.x+1, y: tilePos.y),
+            Pos(x: tilePos.x+0.5, y: tilePos.y+1),
+            Pos(x: tilePos.x-0.5, y: tilePos.y+1)
+        ]
+
+        for pos in positions {
+            if tiles[pos] == nil {
+                tiles[pos] = false
+            }
+        }
     }
 }
--- AdventOfCode/2020/Day_25/Day_25_2020.swift
+++ AdventOfCode/2020/Day_25/Day_25_2020.swift
@@ -12,7 +12,45 @@ enum Day_25_2020: Solvable {
     static var year: Input.Year = .Year_2020
 
     static func solvePart1(input: [Int]) -> String {
-        "Add some Code here"
+        let cardPublicKey = input[0]
+        let doorPublicKey = input[1]
+
+        let subjectNumber = 7
+        let magicValue = 20201227
+
+        var cardLoop = 0
+        var cardValue = 1
+        while cardValue != cardPublicKey {
+            cardValue = cardValue * subjectNumber
+            cardValue = cardValue % magicValue
+            cardLoop += 1
+        }
+
+        var doorLoop = 0
+        var doorValue = 1
+        while doorValue != doorPublicKey {
+            doorValue = doorValue * subjectNumber
+            doorValue = doorValue % magicValue
+            doorLoop += 1
+        }
+
+        var cardEncryptionKey = 1
+        for _ in 0..<doorLoop {
+            cardEncryptionKey = cardEncryptionKey * cardPublicKey
+            cardEncryptionKey = cardEncryptionKey % magicValue
+        }
+
+        var doorEncryptionKey = 1
+        for _ in 0..<cardLoop {
+            doorEncryptionKey = doorEncryptionKey * doorPublicKey
+            doorEncryptionKey = doorEncryptionKey % magicValue
+        }
+
+        if cardEncryptionKey == doorEncryptionKey {
+            return "\(cardEncryptionKey)"
+        } else {
+            return "Something went wrong"
+        }
     }
 
     static func solvePart2(input: [Int]) -> String {
