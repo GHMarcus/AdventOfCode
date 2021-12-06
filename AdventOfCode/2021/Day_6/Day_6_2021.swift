116a0a305253a82e007ba1e1606a14fbb4323270
Marcus Gollnick <mgo@adorsys.de>
2021-12-06 06:59:39 +0100

    Solve Day 6 in 2021

--- AdventOfCode/2021/Day_6/Day_6_2021.swift
+++ AdventOfCode/2021/Day_6/Day_6_2021.swift
@@ -12,10 +12,50 @@ enum Day_6_2021: Solvable {
     static var year: Input.Year = .Year_2021
 
     static func solvePart1(input: [String]) -> String {
-        "Add some Code here"
+        let fishes = input[0].components(separatedBy: ",").compactMap { Int($0) }
+
+        // Days to reproduce from 0 - 8
+        var swarm = Array(repeating: 0, count: 9)
+        for  fish in fishes {
+            swarm[fish] += 1
+        }
+
+        for _ in 1...80 {
+            let reproducingFishes = swarm[0]
+            swarm[0] = 0
+            var newSwarm = Array(repeating: 0, count: 9)
+            for i in 1..<swarm.count {
+                newSwarm[i-1] = swarm[i]
+            }
+            swarm = newSwarm
+            swarm[6] += reproducingFishes
+            swarm[8] += reproducingFishes
+        }
+
+        return  "\(swarm.reduce(0, +))"
     }
 
     static func solvePart2(input: [String]) -> String {
-        return "Add some Code here"
+        let fishes = input[0].components(separatedBy: ",").compactMap { Int($0) }
+
+        // Days to reproduce from 0 - 8
+        var swarm = Array(repeating: 0, count: 9)
+        for  fish in fishes {
+            swarm[fish] += 1
+        }
+
+        for _ in 1...256 {
+            let reproducingFishes = swarm[0]
+            swarm[0] = 0
+            var newSwarm = Array(repeating: 0, count: 9)
+            for i in 1..<swarm.count {
+                newSwarm[i-1] = swarm[i]
+            }
+            swarm = newSwarm
+            swarm[6] += reproducingFishes
+            swarm[8] += reproducingFishes
+        }
+
+        return  "\(swarm.reduce(0, +))"
     }
 }
