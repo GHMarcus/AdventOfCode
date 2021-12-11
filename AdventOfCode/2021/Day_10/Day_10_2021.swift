cd8d376496a5238ab73f65c772923a874d235a90
Marcus Gollnick <mgo@adorsys.de>
2021-12-11 07:58:30 +0100

    Solve Day 10 in 2021

--- AdventOfCode/2021/Day_10/Day_10_2021.swift
+++ AdventOfCode/2021/Day_10/Day_10_2021.swift
@@ -11,11 +11,85 @@ enum Day_10_2021: Solvable {
     static var day: Input.Day = .Day_10
     static var year: Input.Year = .Year_2021
 
+    static var incompleteLines: [String] = []
+
     static func solvePart1(input: [String]) -> String {
-        "Add some Code here"
+
+        var sum = 0
+    line: for line in input {
+        var open = ""
+        for c in Array(line) {
+            switch c {
+            case "(", "[", "{", "<":
+                open.append(c)
+            case ")":
+                let last = open.removeLast()
+                if last != "(" {
+                    sum += 3
+                    continue line
+                }
+            case "]":
+                let last = open.removeLast()
+                if last != "[" {
+                    sum +=  57
+                    continue line
+                }
+            case "}":
+                let last = open.removeLast()
+                if last != "{" {
+                    sum +=  1197
+                    continue line
+                }
+            case ">":
+                let last = open.removeLast()
+                if last != "<" {
+                    sum +=  25137
+                    continue line
+                }
+            default:
+                fatalError()
+            }
+        }
+        incompleteLines.append(line)
+    }
+
+        return "\(sum)"
     }
 
     static func solvePart2(input: [String]) -> String {
-        return "Add some Code here"
+        let allowedChunks = ["()", "[]", "{}", "<>"]
+        var sums: [Int] = []
+
+        for var line in incompleteLines {
+            var newLine = line
+            while true {
+                for chunk in allowedChunks {
+                    newLine = newLine.replacingOccurrences(of: chunk, with: "")
+                }
+                if newLine.count == line.count {
+                    break
+                } else {
+                    line = newLine
+                }
+            }
+            var sum = 0
+            for c in Array(line).reversed() {
+                sum *= 5
+                if c == "(" {
+                    sum += 1
+                } else  if c == "[" {
+                    sum += 2
+                } else  if c == "{" {
+                    sum += 3
+                } else  if c == "<" {
+                    sum += 4
+                }
+            }
+            sums.append(sum)
+        }
+        sums.sort()
+        let middle = Int(sums.count / 2)
+
+        return "\(sums[middle])"
     }
 }
