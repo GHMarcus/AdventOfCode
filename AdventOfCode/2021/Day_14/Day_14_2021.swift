7420e2b30f4b7d549bd01c0771656be811e765d3
Marcus Gollnick <mgo@adorsys.de>
2021-12-14 08:01:36 +0100

    Solve Day 14 Part 1 in 2021

--- AdventOfCode/2021/Day_14/Day_14_2021.swift
+++ AdventOfCode/2021/Day_14/Day_14_2021.swift
@@ -12,10 +12,92 @@ enum Day_14_2021: Solvable {
     static var year: Input.Year = .Year_2021
 
     static func solvePart1(input: [String]) -> String {
-        "Add some Code here"
+        var input = input
+        let template = Array(input.removeFirst())
+        input.removeFirst()
+        var insertions: [(String, Character)] = []
+
+        for line in input {
+            let comps = line.components(separatedBy: " -> ")
+            insertions.append((comps[0], Character(comps[1])))
+        }
+
+        var polymer = template
+        var count = 0
+        while count < 10 {
+            count += 1
+//            print("Step: \(count)")
+            var newPolymer: [String.Element] = []
+
+        out: for pos in 0..<polymer.count {
+            guard pos+1 < polymer.count else {
+                newPolymer.append(polymer[pos])
+                break
+            }
+            let pair = String(polymer[pos]) + String(polymer[pos+1])
+            for insertion in insertions {
+                if pair == insertion.0 {
+                    newPolymer.append(polymer[pos])
+                    newPolymer.append(insertion.1)
+                    continue out
+                }
+            }
+            newPolymer.append(polymer[pos])
+        }
+            polymer = newPolymer
+        }
+
+        let occurrences =  String(polymer).getCountedCharacters.sorted {
+            $0.value > $1.value
+        }
+
+//        print(occurrences)
+
+        return "\(occurrences.first!.value - occurrences.last!.value)"
     }
 
     static func solvePart2(input: [String]) -> String {
-        return "Add some Code here"
+        var input = input
+        let template = Array(input.removeFirst())
+        input.removeFirst()
+        var insertions: [(String, Character)] = []
+
+        for line in input {
+            let comps = line.components(separatedBy: " -> ")
+            insertions.append((comps[0], Character(comps[1])))
+        }
+
+        var polymer = template
+        var count = 0
+        while count < 40 {
+            count += 1
+            print("Step: \(count)")
+            var newPolymer: [String.Element] = []
+
+        out: for pos in 0..<polymer.count {
+            guard pos+1 < polymer.count else {
+                newPolymer.append(polymer[pos])
+                break
+            }
+            let pair = String(polymer[pos]) + String(polymer[pos+1])
+            for insertion in insertions {
+                if pair == insertion.0 {
+                    newPolymer.append(polymer[pos])
+                    newPolymer.append(insertion.1)
+                    continue out
+                }
+            }
+            newPolymer.append(polymer[pos])
+        }
+            polymer = newPolymer
+        }
+
+        let occurrences =  String(polymer).getCountedCharacters.sorted {
+            $0.value > $1.value
+        }
+
+//        print(occurrences)
+
+        return "\(occurrences.first!.value - occurrences.last!.value)"
     }
 }
