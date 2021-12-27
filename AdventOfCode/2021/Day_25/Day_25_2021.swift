7c20268327d78aa48a9dbf8b06e057fb76f15a73
Marcus Gollnick <marcus.gollnick@datev.de>
2021-12-27 15:19:58 +0100

    Solve Day 25 in 2021

--- AdventOfCode/2021/Day_25/Day_25_2021.swift
+++ AdventOfCode/2021/Day_25/Day_25_2021.swift
@@ -12,10 +12,44 @@ enum Day_25_2021: Solvable {
     static var year: Input.Year = .Year_2021
 
     static func solvePart1(input: [String]) -> String {
-        "Add some Code here"
+        var map: [[Character]] = input.map { Array($0) }
+        var newMap: [[Character]] = map
+        var step = 0
+        var someoneMoved = true
+        while someoneMoved {
+            step += 1
+            someoneMoved = false
+            
+            map = newMap
+            for row in 0..<map.count {
+                for col in 0..<map[0].count where map[row][col] == ">" {
+                    
+                    let nextCol = col == map[0].count - 1 ? 0 : col + 1
+                    guard map[row][nextCol] == "." else { continue }
+                    
+                    newMap[row][nextCol] = ">"
+                    newMap[row][col] = "."
+                    someoneMoved = true
+                }
+            }
+            
+            map = newMap
+            for row in 0..<map.count {
+                for col in 0..<map[0].count where map[row][col] == "v" {
+                    
+                    let nextRow = row == map.count - 1 ? 0 : row + 1
+                    guard map[nextRow][col] == "." else { continue }
+                    
+                    newMap[nextRow][col] = "v"
+                    newMap[row][col] = "."
+                    someoneMoved = true
+                }
+            }
+        }
+        return "\(step)"
     }
 
     static func solvePart2(input: [String]) -> String {
-        return "Add some Code here"
+        "Nothing to do here"
     }
 }
--- AdventOfCode/main.swift
+++ AdventOfCode/main.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 let startTime = CFAbsoluteTimeGetCurrent()
-Year_2021().day_21.solve()
+Year_2021().day_25.solve()
 let diffTime = CFAbsoluteTimeGetCurrent() - startTime
 print("Took \(diffTime) seconds")
 
