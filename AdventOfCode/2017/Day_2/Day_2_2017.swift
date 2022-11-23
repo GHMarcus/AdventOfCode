86d62c666516c104b53b839c1df3fa8f4dca1748
Marcus Gollnick <m.gollnick@yahoo.com>
2022-11-23 14:47:20 +0100

    Solve Day 2 in 2017

--- AdventOfCode/2017/Day_2/Day_2_2017.swift
+++ AdventOfCode/2017/Day_2/Day_2_2017.swift
@@ -12,10 +12,43 @@ enum Day_2_2017: Solvable {
     static var year: Input.Year = .Year_2017
 
     static func solvePart1(input: [String]) -> String {
-        return "Add some Code here"
+        var sum = 0
+        
+        for line in input {
+            let numbers = line
+                // filters a random number of spaces and replace it with an `-`
+                .replacingOccurrences(of: "\\s+", with: "-", options: .regularExpression)
+                .components(separatedBy: "-")
+                .map { Int($0)! }
+                .sorted()
+            sum += numbers.last! - numbers.first!
+        }
+        
+        return "\(sum)"
     }
 
     static func solvePart2(input: [String]) -> String {
-        return "Add some Code here"
+        var sum = 0
+        
+        for line in input {
+            let numbers = line
+                // filters a random number of spaces and replace it with an `-`
+                .replacingOccurrences(of: "\\s+", with: "-", options: .regularExpression)
+                .components(separatedBy: "-")
+                .map { Int($0)! }
+            
+            for number in numbers {
+                for innerNumber in numbers {
+                    if number == innerNumber {
+                        continue
+                    } else if number % innerNumber == 0 {
+                        sum += number / innerNumber
+                        break
+                    }
+                }
+            }
+        }
+        
+        return "\(sum)"
     }
 }
