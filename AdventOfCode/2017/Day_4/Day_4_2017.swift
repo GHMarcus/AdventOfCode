0e2f2dc05b19c3be5bcc11af6421e50bf03cc8ac
Marcus Gollnick <m.gollnick@yahoo.com>
2022-11-24 09:08:35 +0100

    Solve Day 4 in 2017

--- AdventOfCode/2017/Day_4/Day_4_2017.swift
+++ AdventOfCode/2017/Day_4/Day_4_2017.swift
@@ -12,10 +12,49 @@ enum Day_4_2017: Solvable {
     static var year: Input.Year = .Year_2017
 
     static func solvePart1(input: [String]) -> String {
-        return "Add some Code here"
+        var validCount = 0
+        
+        for line in input {
+            let phrases = line.components(separatedBy: " ").countedElements
+            var isValid = true
+            for phrase in phrases {
+                if phrase.value > 1 {
+                    isValid = false
+                    break
+                }
+            }
+            if isValid {
+                validCount += 1
+            }
+        }
+        
+        return "\(validCount)"
     }
 
     static func solvePart2(input: [String]) -> String {
-        return "Add some Code here"
+        var validCount = 0
+        
+        for line in input {
+            var phrases = line.components(separatedBy: " ")
+            var isValid = true
+            
+            for _ in 0..<phrases.count {
+                let phrase = Array(phrases.removeFirst()).sorted()
+                phrases.append(String(phrase))
+            }
+            
+            let countedPhrases = phrases.countedElements
+            for phrase in countedPhrases {
+                if phrase.value > 1 {
+                    isValid = false
+                    break
+                }
+            }
+            if isValid {
+                validCount += 1
+            }
+        }
+        
+        return "\(validCount)"
     }
 }
