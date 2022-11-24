f7c41220fec90023f5009f754bfe379af3238d24
Marcus Gollnick <m.gollnick@yahoo.com>
2022-11-24 10:23:53 +0100

    Solve Day 5 in 2017

--- AdventOfCode/2017/Day_5/Day_5_2017.swift
+++ AdventOfCode/2017/Day_5/Day_5_2017.swift
@@ -11,11 +11,47 @@ enum Day_5_2017: Solvable {
     static var day: Input.Day = .Day_5
     static var year: Input.Year = .Year_2017
 
-    static func solvePart1(input: [String]) -> String {
-        return "Add some Code here"
+    static func solvePart1(input: [Int]) -> String {
+        var instructions = input
+        var pointer = 0
+        var steps = 0
+        
+        while true {
+            if pointer >= instructions.count || pointer < 0 {
+                break
+            }
+            steps += 1
+            var current = instructions[pointer]
+            let nextPointer = pointer + current
+            current += 1
+            instructions[pointer] = current
+            pointer = nextPointer
+        }
+        
+        return "\(steps)"
     }
 
-    static func solvePart2(input: [String]) -> String {
-        return "Add some Code here"
+    static func solvePart2(input: [Int]) -> String {
+        var instructions = input
+        var pointer = 0
+        var steps = 0
+        
+        while true {
+            if pointer >= instructions.count || pointer < 0 {
+                break
+            }
+            steps += 1
+            var current = instructions[pointer]
+            let nextPointer = pointer + current
+            if current >= 3 {
+                current -= 1
+            } else {
+                current += 1
+            }
+            instructions[pointer] = current
+            pointer = nextPointer
+        }
+        
+        return "\(steps)"
     }
 }
