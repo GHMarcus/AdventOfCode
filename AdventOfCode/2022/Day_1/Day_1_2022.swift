f672a18f34b52f1f3f60b919c45d462c408a21b6
Marcus Gollnick <m.gollnick@yahoo.com>
2022-12-01 11:39:52 +0100

    Fix typo and do a little refactoring

--- AdventOfCode/2022/Day_1/Day_1_2022.swift
+++ AdventOfCode/2022/Day_1/Day_1_2022.swift
@@ -12,43 +12,36 @@ enum Day_1_2022: Solvable {
     static var year: Input.Year = .Year_2022
 
     static func solvePart1(input: [String]) -> String {
-        var calories: [Int] = []
-
-        var currentElfsCalorien = 0
-        for line in input {
-            if line.isEmpty {
-                calories.append(currentElfsCalorien)
-                currentElfsCalorien = 0
-                continue
-            }
-
-            currentElfsCalorien += Int(line)!
-        }
-        calories.append(currentElfsCalorien)
-
+        var calories = sumUpCaloriesPerElf(for: input)
 
         return "\(calories.max()!)"
     }
 
     static func solvePart2(input: [String]) -> String {
-        var calories: [Int] = []
-
-        var currentElfsCalorien = 0
-        for line in input {
-            if line.isEmpty {
-                calories.append(currentElfsCalorien)
-                currentElfsCalorien = 0
-                continue
-            }
-
-            currentElfsCalorien += Int(line)!
-        }
-        calories.append(currentElfsCalorien)
-
+        var calories = sumUpCaloriesPerElf(for: input)
+        
         calories.sort { $0 > $1 }
 
         let total = calories.prefix(3).reduce(0, +)
 
         return "\(total)"
+    }
+    
+    private static func sumUpCaloriesPerElf(for input: [String]) -> [Int] {
+        var calories: [Int] = []
+        var currentElfsCalories = 0
+        var input = input
+        // To collect the last sum automatically
+        input.append("")
+        for line in input {
+            if line.isEmpty {
+                calories.append(currentElfsCalories)
+                currentElfsCalories = 0
+                continue
+            }
+
+            currentElfsCalories += Int(line)!
+        }
+        return calories
     }
 }
