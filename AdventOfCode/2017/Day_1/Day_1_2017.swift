afd86331e953912a4bf9607265533ab6cd4b41f2
Marcus Gollnick <m.gollnick@yahoo.com>
2022-12-01 11:24:51 +0100

    Rebase Day1 in 2017

--- AdventOfCode/2017/Day_1/Day_1_2017.swift
+++ AdventOfCode/2017/Day_1/Day_1_2017.swift
@@ -11,11 +11,35 @@ enum Day_1_2017: Solvable {
     static var day: Input.Day = .Day_1
     static var year: Input.Year = .Year_2017
 
-    static func solvePart1(input: [String]) -> String {
-        return "Add some Code here"
+    static func solvePart1(input: [Character]) -> String {
+        var numbers = input.map { Int(String($0))! }
+        let first = numbers.first!
+        numbers.append(first)
+        
+        var sum = 0
+        for i in 0..<numbers.count-1 {
+            let current = numbers[i]
+            if current == numbers[i+1] {
+                sum += current
+            }
+        }
+        
+        return "\(sum)"
     }
 
-    static func solvePart2(input: [String]) -> String {
-        return "Add some Code here"
+    static func solvePart2(input: [Character]) -> String {
+        let numbers = input.map { Int(String($0))! }
+        let offset = numbers.count / 2
+        
+        var sum = 0
+        for i in 0..<numbers.count {
+            let current = numbers[i]
+            let next = i + offset > numbers.count - 1 ? (i + offset - numbers.count) : (i + offset)
+            if current == numbers[next] {
+                sum += current
+            }
+        }
+        
+        return "\(sum)"
     }
 }
