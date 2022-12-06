470b51b89b97d4d079f6663a9b8876b919734ff7
Marcus Gollnick <mgo@adorsys.de>
2022-12-06 09:29:51 +0100

    Solve Day 6 in 2022

--- AdventOfCode/2022/Day_6/Day_6_2022.swift
+++ AdventOfCode/2022/Day_6/Day_6_2022.swift
@@ -11,11 +11,30 @@ enum Day_6_2022: Solvable {
     static var day: Input.Day = .Day_6
     static var year: Input.Year = .Year_2022
 
-    static func solvePart1(input: [String]) -> String {
-        return "Add some Code here"
+    static func solvePart1(input: [Character]) -> String {
+        return "\(findMarker(length: 4, input: input))"
     }
 
-    static func solvePart2(input: [String]) -> String {
-        return "Add some Code here"
+    static func solvePart2(input: [Character]) -> String {
+        return "\(findMarker(length: 14, input: input))"
+    }
+
+    static func findMarker(length: Int, input: [Character]) -> Int {
+        var i = 0
+        var length = length - 1
+
+        while i+length < input.count {
+            let marker = input[i...i+length]
+            let isRealMarker = String(marker).getCountedCharacters.allSatisfy { $0.value == 1 }
+            if isRealMarker {
+                // length for the offset of characters and 1 for array starting with 0
+                i += length + 1
+                break
+            } else {
+                i += 1
+            }
+        }
+
+        return i
     }
 }
--- AdventOfCode/main.swift
+++ AdventOfCode/main.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 let startTime = CFAbsoluteTimeGetCurrent()
-Year_2022().day_5.solve()
+Year_2022().day_6.solve()
 let diffTime = CFAbsoluteTimeGetCurrent() - startTime
 print("Took \(diffTime) seconds")
 
