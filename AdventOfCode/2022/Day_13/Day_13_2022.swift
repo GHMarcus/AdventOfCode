1440d75ab78ae7984e6d1bbd74ad8a8810bf7f58
Marcus Gollnick <m.gollnick@yahoo.com>
2022-12-13 15:39:50 +0100

    Solve Day 13 in 2022

--- AdventOfCode/2022/Day_13/Day_13_2022.swift
+++ AdventOfCode/2022/Day_13/Day_13_2022.swift
@@ -10,147 +10,151 @@
 enum Day_13_2022: Solvable {
     static var day: Input.Day = .Day_13
     static var year: Input.Year = .Year_2022
+    
+    class Signal {
+        init(own: String) {
+            self.own = own
+        }
+        enum Kind {
+            case integer, array
+        }
+        
+        var own: String
+        var subParts: [Signal] = []
+        
+        var kind: Kind {
+            if own.contains("[") {
+                return .array
+            } else {
+                return .integer
+            }
+        }
+    }
 
     static func solvePart1(input: [String]) -> String {
         var rightOrders: [Int] = []
         var orderNumber = 1
-        
-        for l in stride(from: 0, to: input.count, by: 3) {
-            let leftSide = input[l]
-            let rightSide = input[l+1]
 
-            if checkOrderFor(left: leftSide, right: rightSide) {
+        var checkPairs: [(left: Signal, right: Signal)] = []
+        for l in stride(from: 0, to: input.count, by: 3) {
+            let leftSignal = createSignal(for: input[l])
+            let rightSignal = createSignal(for: input[l+1])
+            
+            checkPairs.append((leftSignal, rightSignal))
+        }
+        
+        for pair in checkPairs {
+            if let value = checkSignalOrder(pair.left, pair.right), value {
                 rightOrders.append(orderNumber)
             }
             orderNumber += 1
         }
 
-        print("*********************************")
-        print(rightOrders)
-
         return "\(rightOrders.reduce(0, +))"
     }
 
     static func solvePart2(input: [String]) -> String {
-        return "Add some Code here"
-    }
+        let divider1 = "[[2]]"
+        let divider2 = "[[6]]"
+        var input = input
+        input.append(contentsOf: ["", divider1, divider2])
 
-    static func checkOrderFor(left: String, right: String) -> Bool {
-        if left.countInstances(of: "[") == right.countInstances(of: "[") {
-//            print("------------------------------")
-            let leftInts = left
-                .replacingOccurrences(of: "[", with: " ")
-                .replacingOccurrences(of: "]", with: " ")
-                .replacingOccurrences(of: ",", with: " ")
-                .components(separatedBy: " ")
-                .compactMap { Int($0) }
-//            print(leftInts)
-
-            let rightInts = right
-                .replacingOccurrences(of: "[", with: " ")
-                .replacingOccurrences(of: "]", with: " ")
-                .replacingOccurrences(of: ",", with: " ")
-                .components(separatedBy: " ")
-                .compactMap { Int($0) }
-            //            print(rightInts)
-
-            var isRightOrder = true
-            for n in 0..<leftInts.count {
-                // right side run out of elements
-                if n >= rightInts.count {
-                    isRightOrder = false
-                    break
-                }
-                // left side is bigger, so stop comparing
-                if leftInts[n] > rightInts[n] {
-                    isRightOrder = false
-                    break
-                }
-
-                if leftInts[n] < rightInts[n] {
-                    isRightOrder = true
-                    break
-                }
-            }
-            return isRightOrder
-        } else {
-            var leftParts: [String] = []
-            var rightParts: [String] = []
-
-            var leftArr = Array(left)
-            var rightArr = Array(right)
-
-            func getNextPartFrom(arr: [Character]) -> (part: String, rest: [Character]) {
-                var openingBrs: [Int] = []
-
-                var part = ""
-                var rest = arr
-
-
-                for c in 0..<arr.count {
-                    if arr[c] == "[" {
-                        openingBrs.append(c)
-                    } else if arr[c] == "]" {
-                        let openingBr = openingBrs.removeLast()
-                        part = String(arr[openingBr...c])
-                        if #available(macOS 13.0, *) {
-                            rest = arr.replacing(arr[openingBr...c], with: [])
-                        } else {
-                            fatalError("Update your mac")
-                        }
-                        break
-                    }
-                }
-
-                return (part, rest)
-            }
-
-            while true {
-                if leftArr.isEmpty {
-                    break
-                }
-                let next = getNextPartFrom(arr: leftArr)
-                leftParts.append(next.part)
-                leftArr = next.rest
-            }
-
-            while true {
-                if rightArr.isEmpty {
-                    break
-                }
-                let next = getNextPartFrom(arr: rightArr)
-                rightParts.append(next.part)
-                rightArr = next.rest
-            }
-
-            var isRightOrder = true
-
-//            print("+++++++++++++++++++++++++++++")
-//            print(leftParts)
-//            print(rightParts)
-//            print(left)
-//            print(right)
-
-            for n in 0..<leftParts.count {
-                if leftParts[n] == "[,]" {
-                    continue
-                }
-                if n >= rightParts.count {
-                    isRightOrder = false
-                    break
-                }
-                isRightOrder = isRightOrder && checkOrderFor(left: leftParts[n], right: rightParts[n])
-            }
-
-
-
-            return isRightOrder
+        var signals: [Signal] = []
+        for l in stride(from: 0, to: input.count, by: 3) {
+            let leftSignal = createSignal(for: input[l])
+            let rightSignal = createSignal(for: input[l+1])
+            
+            signals.append(contentsOf: [leftSignal, rightSignal])
         }
+        
+        signals = signals.sorted {
+            checkSignalOrder($0, $1) ?? false
+        }
+        
+        let divider1Index = Int(signals.firstIndex { $0.own == divider1 }!)
+        let divider2Index = Int(signals.firstIndex { $0.own == divider2 }!)
 
-
-        return false
+        return "\((divider1Index + 1) * (divider2Index + 1))"
+    }
+    
+    static func checkSignalOrder(_ left: Signal, _ right: Signal) -> Bool? {
+        switch (left.kind, right.kind) {
+        case (.integer, .integer):
+            if let leftInt = Int(left.own), let rightInt = Int(right.own), leftInt < rightInt {
+                return true
+            } else if let leftInt = Int(left.own), let rightInt = Int(right.own), leftInt > rightInt  {
+                return false
+            } else {
+                return nil
+            }
+        case (.integer, .array):
+            let leftSubPart = Signal(own: left.own)
+            left.subParts.append(leftSubPart)
+            left.own = "[" + left.own + "]"
+            return checkSignalOrder(left, right)
+        case (.array, .integer):
+            let rightSubPart = Signal(own: right.own)
+            right.subParts.append(rightSubPart)
+            right.own = "[" + right.own + "]"
+            return checkSignalOrder(left, right)
+        case (.array, .array):
+            if left.subParts[0].own.isEmpty, !right.subParts[0].own.isEmpty {
+                return true
+            }
+            if right.subParts[0].own.isEmpty, !left.subParts[0].own.isEmpty {
+                return false
+            }
+            for n in 0..<left.subParts.count {
+                if n >= right.subParts.count {
+                    return false
+                }
+                if let value = checkSignalOrder(left.subParts[n], right.subParts[n]) {
+                    return value
+                }
+            }
+            if left.subParts.count < right.subParts.count {
+                return true
+            }
+        }
+        
+        return nil
+    }
+    
+    static func createSignal(for string: String) -> Signal {
+        let signal = Signal(own: string)
+        
+        guard string.contains("[") else {
+            return signal
+        }
+        // Get rid of the outer most brackets
+        var parts = Array(string)
+        parts.removeLast()
+        parts.removeFirst()
+        
+        var subparts: [String] = []
+        var currentPart: [Character] = []
+        var openingInnerBrackets = 0
+        for c in parts {
+            if c == ",", openingInnerBrackets == 0 {
+                subparts.append(String(currentPart))
+                currentPart = []
+            } else if c == "[" {
+                openingInnerBrackets += 1
+                currentPart.append(c)
+            } else if c == "]" {
+                openingInnerBrackets -= 1
+                currentPart.append(c)
+            } else {
+                currentPart.append(c)
+            }
+        }
+        
+        subparts.append(String(currentPart))
+        
+        signal.subParts = subparts.map{
+            createSignal(for: $0)
+        }
+        
+        return signal
     }
 }
-
-// [5, 31, 42, 50, 69, 72, 76, 83, 104, 126, 146]
-// 804 -> low
