54cf8e8c21d094c3a1b3af00cfe0256691cbe708
Marcus Gollnick <m.gollnick@yahoo.com>
2022-12-14 08:44:59 +0100

    ReSolve Day 14 in 2022

--- AdventOfCode/2022/Day_14/Day_14_2022.swift
+++ AdventOfCode/2022/Day_14/Day_14_2022.swift
@@ -10,57 +10,35 @@
 enum Day_14_2022: Solvable {
     static var day: Input.Day = .Day_14
     static var year: Input.Year = .Year_2022
-
-    static func solvePart1(input: [String]) -> String {
-        var map: [[Character]] = []
-        var rocks: [(x: Int, y: Int)] = []
-
-        var maxX = 0
-        var maxY = 0
-
-        var minX = Int.max
-
-        for line in input {
-            let cmp = line.components(separatedBy: " -> ")
-
-            var previousX: Int?
-            var previousY: Int?
-
-            for c in cmp {
-                let coords = c.components(separatedBy: ",").map { Int($0)! }
-
-                if let pX = previousX, let pY = previousY {
-                    let currentX = coords[0]
-                    let currentY = coords[1]
-                    if pX == currentX {
-                        let stride = pY < currentY
-                        ? pY ... currentY
-                        : currentY ... pY
-                        for y in stride {
-                            rocks.append((currentX, y))
-                        }
-                    } else if pY == currentY {
-                        let stride = pX < currentX
-                        ? pX ... currentX
-                        : currentX ... pX
-                        for x in stride {
-                            rocks.append((x, currentY))
-                        }
-                    }
-                    previousX = currentX
-                    previousY = currentY
-                } else {
-                    previousX = coords[0]
-                    previousY = coords[1]
-                }
-
-                maxY = coords[1] > maxY ? coords[1] : maxY
-                maxX = coords[0] > maxX ? coords[0] : maxX
-                minX = coords[0] < minX ? coords[0] : minX
-            }
+    
+    struct Point {
+        enum Direction: String {
+            case down, left, right
         }
 
+        var x, y: Int
+        var nextDirection: Direction
+        
+        mutating func move() {
+            switch nextDirection {
+            case .down:
+                y += 1
+            case .left:
+                x -= 1
+            case .right:
+                x += 1
+            }
+        }
+    }
 
+    static func solvePart1(input: [String]) -> String {
+        var maxX = 0
+        var maxY = 0
+        var minX = Int.max
+        
+        let rocks = convert(input, maxX: &maxX, maxY: &maxY, minX: &minX)
+
+        var map: [[Character]] = []
         map = Array(repeating: Array(repeating: ".", count: maxX+1), count: maxY+1)
 
         rocks.forEach {
@@ -69,42 +47,15 @@ enum Day_14_2022: Solvable {
 
         map[0][500] = "+"
 
-        var round = 0
-
-
-
-        struct Point {
-            enum Direction: String {
-                case down, left, right
-            }
-
-            var x, y: Int
-            var nextDirection: Direction
-
-            func pr() -> String {
-                "x:\(x), y:\(y), next:\(nextDirection.rawValue)"
-            }
-        }
-
         var openPoints: [Point?] = [Point(x: 500, y: 0, nextDirection: .down)]
 
-        print(maxY)
-
-        while true { //round < 1000 {
+        while true {
             let lastOpenPoint = openPoints.popLast() ?? nil
             guard var nextPoint = lastOpenPoint else {
-                print("lastOne \(round)")
                 break
             }
 
-            switch nextPoint.nextDirection {
-            case .down:
-                nextPoint.y += 1
-            case .left:
-                nextPoint.x -= 1
-            case .right:
-                nextPoint.x += 1
-            }
+            nextPoint.move()
 
             if nextPoint.x >= map[0].count || nextPoint.y >= map.count || nextPoint.x < 0 {
                 break
@@ -113,43 +64,23 @@ enum Day_14_2022: Solvable {
             let next = map[nextPoint.y][nextPoint.x]
 
             if next == "." {
-//                print("\(nextPoint.pr()) -> \(next) -> 1")
                 nextPoint.nextDirection = .down
                 openPoints.append(nextPoint)
             } else if next == "#" || next == "o" {
                 switch nextPoint.nextDirection {
                 case .down:
-//                    print("\(nextPoint.pr()) -> \(next) -> 2")
                     nextPoint.nextDirection = .left
                     openPoints.append(nextPoint)
                 case .left:
-//                    print("\(nextPoint.pr()) -> \(next) -> 3")
                     nextPoint.nextDirection = .right
                     nextPoint.x += 1
                     openPoints.append(nextPoint)
                 case .right:
-//                    print("\(nextPoint.pr()) -> \(next) -> 4")
                     map[nextPoint.y-1][nextPoint.x-1] = "o"
                     openPoints.append(Point(x: 500, y: 0, nextDirection: .down))
                     continue
                 }
             }
-
-            round += 1
-
-//            for y in 0...(maxY+1) {
-//                var str = ""
-//                for x in (minX-1)...(maxX+1) {
-//
-//                    if x == nextPoint.x && y == nextPoint.y {
-//                        str.append("o")
-//                    } else {
-//                        str.append(map[y][x])
-//                    }
-//                }
-//                print(str)
-//            }
-//            print("---------------------------")
         }
 
         var sand = 0
@@ -157,104 +88,39 @@ enum Day_14_2022: Solvable {
         for row in map {
             sand += row.countedElements["o"] ?? 0
         }
-
-
+        
         return "\(sand)"
     }
 
     static func solvePart2(input: [String]) -> String {
-        var map: [[Character]] = []
-        var rocks: [(x: Int, y: Int)] = []
-
         var maxX = 0
         var maxY = 0
-
         var minX = Int.max
-
-        for line in input {
-            let cmp = line.components(separatedBy: " -> ")
-
-            var previousX: Int?
-            var previousY: Int?
-
-            for c in cmp {
-                let coords = c.components(separatedBy: ",").map { Int($0)! }
-
-                if let pX = previousX, let pY = previousY {
-                    let currentX = coords[0]
-                    let currentY = coords[1]
-                    if pX == currentX {
-                        let stride = pY < currentY
-                        ? pY ... currentY
-                        : currentY ... pY
-                        for y in stride {
-                            rocks.append((currentX, y))
-                        }
-                    } else if pY == currentY {
-                        let stride = pX < currentX
-                        ? pX ... currentX
-                        : currentX ... pX
-                        for x in stride {
-                            rocks.append((x, currentY))
-                        }
-                    }
-                    previousX = currentX
-                    previousY = currentY
-                } else {
-                    previousX = coords[0]
-                    previousY = coords[1]
-                }
-
-                maxY = coords[1] > maxY ? coords[1] : maxY
-                maxX = coords[0] > maxX ? coords[0] : maxX
-                minX = coords[0] < minX ? coords[0] : minX
-            }
-        }
-
-
+        
+        let rocks = convert(input, maxX: &maxX, maxY: &maxY, minX: &minX)
+        
+        var map: [[Character]] = []
         map = Array(repeating: Array(repeating: ".", count: maxX+maxX), count: maxY+3)
 
         rocks.forEach {
             map[$0.y][$0.x] = "#"
         }
+        
+        for x in 0..<map[0].count {
+            map[map.count-1][x] = "#"
+        }
 
         map[0][500] = "+"
 
-        var round = 0
-
-
-
-        struct Point {
-            enum Direction: String {
-                case down, left, right
-            }
-
-            var x, y: Int
-            var nextDirection: Direction
-
-            func pr() -> String {
-                "x:\(x), y:\(y), next:\(nextDirection.rawValue)"
-            }
-        }
-
         var openPoints: [Point?] = [Point(x: 500, y: 0, nextDirection: .down)]
         var sourceIsFree = true
-//        while sourceIsFree { //round < 1000 {
-        while round < 1000 {
+        while sourceIsFree {
             let lastOpenPoint = openPoints.popLast() ?? nil
             guard var nextPoint = lastOpenPoint else {
-                print("lastOne \(round)")
                 break
             }
 
-            switch nextPoint.nextDirection {
-            case .down:
-                nextPoint.y += 1
-            case .left:
-                nextPoint.x -= 1
-            case .right:
-                nextPoint.x += 1
-            }
+            nextPoint.move()
 
             if nextPoint.x >= map[0].count || nextPoint.y >= map.count || nextPoint.x < 0 {
                 openPoints.append(Point(x: 500, y: 0, nextDirection: .down))
@@ -264,46 +130,26 @@ enum Day_14_2022: Solvable {
             let next = map[nextPoint.y][nextPoint.x]
 
             if next == "." {
-//                print("\(nextPoint.pr()) -> \(next) -> 1")
                 nextPoint.nextDirection = .down
                 openPoints.append(nextPoint)
             } else if next == "#" || next == "o" {
                 switch nextPoint.nextDirection {
                 case .down:
-//                    print("\(nextPoint.pr()) -> \(next) -> 2")
                     nextPoint.nextDirection = .left
                     openPoints.append(nextPoint)
                 case .left:
-//                    print("\(nextPoint.pr()) -> \(next) -> 3")
                     nextPoint.nextDirection = .right
                     nextPoint.x += 1
                     openPoints.append(nextPoint)
                 case .right:
-//                    print("\(nextPoint.pr()) -> \(next) -> 4")
                     map[nextPoint.y-1][nextPoint.x-1] = "o"
                     openPoints.append(Point(x: 500, y: 0, nextDirection: .down))
-                    if nextPoint.x == 500, nextPoint.y == 0 {
+                    if map[0][500] == "o" {
                         sourceIsFree = false
                     }
                     continue
                 }
             }
-
-            round += 1
-
-            for y in 0...(maxY) {
-                var str = ""
-                for x in (minX-1)...(maxX) {
-
-                    if x == nextPoint.x && y == nextPoint.y {
-                        str.append("o")
-                    } else {
-                        str.append(map[y][x])
-                    }
-                }
-                print(str)
-            }
-            print("---------------------------")
         }
 
         var sand = 0
@@ -312,7 +158,52 @@ enum Day_14_2022: Solvable {
             sand += row.countedElements["o"] ?? 0
         }
 
-
         return "\(sand)"
+    }
+    
+    static func convert(_ input: [String], maxX: inout Int, maxY: inout Int, minX: inout Int) -> [(x: Int, y: Int)] {
+        var rocks: [(x: Int, y: Int)] = []
+        
+        for line in input {
+            let cmp = line.components(separatedBy: " -> ")
+
+            var previousX: Int?
+            var previousY: Int?
+
+            for c in cmp {
+                let coords = c.components(separatedBy: ",").map { Int($0)! }
+
+                if let pX = previousX, let pY = previousY {
+                    let currentX = coords[0]
+                    let currentY = coords[1]
+                    if pX == currentX {
+                        let stride = pY < currentY
+                        ? pY ... currentY
+                        : currentY ... pY
+                        for y in stride {
+                            rocks.append((currentX, y))
+                        }
+                    } else if pY == currentY {
+                        let stride = pX < currentX
+                        ? pX ... currentX
+                        : currentX ... pX
+                        for x in stride {
+                            rocks.append((x, currentY))
+                        }
+                    }
+                    previousX = currentX
+                    previousY = currentY
+                } else {
+                    previousX = coords[0]
+                    previousY = coords[1]
+                }
+
+                maxY = coords[1] > maxY ? coords[1] : maxY
+                maxX = coords[0] > maxX ? coords[0] : maxX
+                minX = coords[0] < minX ? coords[0] : minX
+            }
+        }
+        
+        return rocks
     }
 }
