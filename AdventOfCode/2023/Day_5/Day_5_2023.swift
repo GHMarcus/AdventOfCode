99b5388e92db0e760b6926a96a66b607544a6811
Marcus Gollnick <m.gollnick@yahoo.com>
2023-12-05 12:39:38 +0100

    Make it Day 5 in 2023 faster but ugly

--- AdventOfCode/2023/Day_5/Day_5_2023.swift
+++ AdventOfCode/2023/Day_5/Day_5_2023.swift
@@ -12,7 +12,7 @@ enum Day_5_2023: Solvable {
     static var year: Input.Year = .Year_2023
     
     struct Ranges {
-        let destination: ClosedRange<Int>
+        let offset: Int
         let source: ClosedRange<Int>
         
         init(_ str: String) {
@@ -20,14 +20,18 @@ enum Day_5_2023: Solvable {
             let destinationStart = Int(cmp[0])!
             let sourceStart = Int(cmp[1])!
             let length = Int(cmp[2])!
-            destination = destinationStart ... destinationStart + length - 1
             source = sourceStart ... sourceStart + length - 1
+            offset = destinationStart - sourceStart
+        }
+        
+        init(source: ClosedRange<Int>, offset: Int) {
+            self.source = source
+            self.offset = offset
         }
         
         func convert(_ seed: Int) -> Int? {
             guard source.contains(seed) else { return nil }
-            let distance = seed - source.lowerBound
-            return destination.lowerBound + distance
+            return seed+offset
         }
     }
     
@@ -48,6 +52,45 @@ enum Day_5_2023: Solvable {
             }
             return location
         }
+        
+        func findLocations(for initialSeedRange: ClosedRange<Int>) -> ClosedRange<Int> {
+            var seedRanges: Set<ClosedRange<Int>> = [initialSeedRange]
+            
+            for section in maps {
+                seedRanges = convert(seedRanges, in: section)
+            }
+            
+            return seedRanges.sorted { $0.lowerBound < $1.lowerBound }.first!
+        }
+        
+        func convert(_ seedRanges: Set<ClosedRange<Int>>, in section: [Ranges]) -> Set<ClosedRange<Int>> {
+            
+            var newSeedRanges: Set<ClosedRange<Int>> = []
+            
+            for seedRange in seedRanges {
+                var convertedRanges: [Ranges] = []
+                let fittingRanges = section.filter { seedRange.overlaps($0.source) }
+                
+                for fittingRange in fittingRanges {
+                    let range = fittingRange.source.clamped(to: seedRange)
+                    convertedRanges.append(Ranges(source: range, offset: fittingRange.offset))
+                }
+                
+                convertedRanges.sort { $0.source.lowerBound < $1.source.lowerBound }
+                if convertedRanges.isEmpty {
+                    convertedRanges = [Ranges(source: seedRange, offset: 0)]
+                } else if seedRange.lowerBound < convertedRanges.first!.source.lowerBound {
+                    convertedRanges.insert(Ranges(source: seedRange.lowerBound ... convertedRanges.first!.source.lowerBound-1, offset: 0), at: 0)
+                } else  if seedRange.upperBound > convertedRanges.last!.source.upperBound {
+                    convertedRanges.append(Ranges(source: convertedRanges.last!.source.upperBound+1 ... seedRange.upperBound, offset: 0))
+                }
+                
+                let tmp = convertedRanges.map { ($0.source.lowerBound+$0.offset) ... ($0.source.upperBound+$0.offset) }
+                tmp.forEach { newSeedRanges.insert($0)}
+            }
+            
+            return newSeedRanges
+        }
     }
     
     static func convert(input: [String]) -> Almanac {
@@ -61,8 +104,8 @@ enum Day_5_2023: Solvable {
         let seedLine = lines.removeFirst()
         seedsPart1 = seedLine.components(separatedBy: ": ")[1].components(separatedBy: " ").map({ Int($0)! })
         seedsPart2 = seedsPart1.chunked(into: 2).map { $0.first!...$0.first!+$0.last!-1 }.sorted {$0.lowerBound < $1.lowerBound}
-        
-        var jumpOverNextLine = false
+        lines.removeFirst()
+        var jumpOverNextLine = true
         for line in lines {
             if jumpOverNextLine {
                 jumpOverNextLine = false
@@ -96,111 +139,12 @@ enum Day_5_2023: Solvable {
 
     static func solvePart2(input: Almanac) -> String {
         var minLocation = Int.max
-        for (index, seedRange) in input.seedsPart2.enumerated() {
-            print("Range #\(index)")
-            for seed in seedRange {
-                minLocation = min(input.convert(seed), minLocation)
-            }
+        
+        for seedRange in input.seedsPart2 {
+            let locations = input.findLocations(for: seedRange)
+            minLocation = min(minLocation, locations.lowerBound)
         }
         
         return "\(minLocation)"
     }
 }
-
-
-
-
-//enum Day_5_2023: Solvable {
-//    static var day: Input.Day = .Day_5
-//    static var year: Input.Year = .Year_2023
-//    
-//    struct MapRange {
-//        var destination: Int
-//        var source: Int
-//        var rangeLength: Int
-//        
-//        func Map(value: Int) -> Int? {
-//            var result: Int?
-//            if value >= source && value < source + rangeLength {
-//                result = value - source + destination
-//            }
-//            return result
-//        }
-//        
-//        init(str: String) {
-//            let s = str.components(separatedBy: .whitespaces)
-//            destination = Int(s[0])!
-//            source = Int(s[1])!
-//            rangeLength = Int(s[2])!
-//        }
-//    }
-//
-//    struct Map {
-//        var mapRanges: [MapRange] = []
-//        
-//        func convert(value: Int) -> Int {
-//            for map in mapRanges {
-//                if let result = map.Map(value: value) {
-//                    return result
-//                }
-//            }
-//            return value
-//        }
-//    }
-//    
-//    static func convert(input: [String]) -> ([Int], [Map]) {
-//        var maps: [Map] = []
-//        var lineIndex = 0
-//        
-//        let seeds: [Int] = input[lineIndex].components(separatedBy: .whitespaces).dropFirst().map( { Int($0)! } )
-//        lineIndex += 3
-//        
-//        while lineIndex < input.count {
-//            var m = Map()
-//            while lineIndex < input.count && input[lineIndex] != "" {
-//                m.mapRanges.append(MapRange(str: input[lineIndex]))
-//                lineIndex += 1
-//            }
-//            maps.append(m)
-//            lineIndex += 2
-//        }
-//        return (seeds, maps)
-//    }
-//    
-//    static func solvePart1(input: ([Int], [Map])) -> String {
-//        var result = Int.max
-//        var seeds: [Int] = input.0
-//        var maps: [Map] = input.1
-//        
-//        for seed in seeds {
-//            var x = seed
-//            for map in maps {
-//                x = map.convert(value: x)
-//            }
-//            result = min(result, x)
-//        }
-//        return "\(result)"
-//    }
-//    
-//    static func solvePart2(input: ([Int], [Map])) -> String {
-//        var result = Int.max
-//        var seeds: [Int] = input.0
-//        var maps: [Map] = input.1
-//        
-//        var seedIndex = 0
-//        while seedIndex < seeds.count {
-//            let start = seeds[seedIndex]
-//            let range = seeds[seedIndex+1]
-//            seedIndex += 2
-//            for seed in (start...start+range-1) {
-//                var x = seed
-//                for map in maps {
-//                    x = map.convert(value: x)
-//                }
-//                result = min(result, x)
-//            }
-//        }
-//        
-//        return "\(result)"
-//    }
-//}
