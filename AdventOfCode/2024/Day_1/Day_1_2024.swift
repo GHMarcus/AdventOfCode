42c328d3e99fbd07609be1bb1dc3d8edf1cc5535
Marcus Gollnick <marcus.gollnick@datev.de>
2024-12-01 10:45:23 +0100

    Solve Day 1 in 2024

--- AdventOfCode/2024/Day_1/Day_1_2024.swift
+++ AdventOfCode/2024/Day_1/Day_1_2024.swift
@@ -11,15 +11,42 @@ enum Day_1_2024: Solvable {
     static var day: Input.Day = .Day_1
     static var year: Input.Year = .Year_2024
 
-    static func convert(input: [String]) -> [String] {
-        input
+    static func convert(input: [String]) -> (left: [Int], right: [Int]) {
+        var leftList: [Int] = []
+        var rightList: [Int] = []
+        
+        for line in input {
+            let cmp = line.components(separatedBy: "   ")
+            leftList.append(Int(cmp[0])!)
+            rightList.append(Int(cmp[1])!)
+        }
+        
+        return (left: leftList, right: rightList)
     }
 
-    static func solvePart1(input: [String]) -> String {
-        return "Add some Code here"
+    static func solvePart1(input: (left: [Int], right: [Int])) -> String {
+        
+        let sortedLeft = input.left.sorted()
+        let sortedRight = input.right.sorted()
+        
+        var sumOfDistances: Int = 0
+        
+        for n in 0..<sortedLeft.count {
+            sumOfDistances += abs(sortedLeft[n] - sortedRight[n])
+        }
+        
+        return "\(sumOfDistances)"
     }
 
-    static func solvePart2(input: [String]) -> String {
-        return "Add some Code here"
+    static func solvePart2(input: (left: [Int], right: [Int])) -> String {
+        
+        let countedRightList = input.right.countedElements
+        var similarityScore: Int = 0
+        
+        for i in input.left {
+            similarityScore += (countedRightList[i] ?? 0) * i
+        }
+        
+        return "\(similarityScore)"
     }
 }
