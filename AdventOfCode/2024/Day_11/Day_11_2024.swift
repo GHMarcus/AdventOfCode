33f2dd63fc6b3161e98843a7b095ee472e15e8ad
Marcus Gollnick <marcus.gollnick@datev.de>
2024-12-11 14:08:51 +0100

    Solve Day 11 in 2024

--- AdventOfCode/2024/Day_11/Day_11_2024.swift
+++ AdventOfCode/2024/Day_11/Day_11_2024.swift
@@ -10,17 +10,46 @@
 enum Day_11_2024: Solvable {
     static var day: Input.Day = .Day_11
     static var year: Input.Year = .Year_2024
-    typealias ConvertedInput = [String]
+    typealias ConvertedInput = [Int: Int]
 
     static func convert(input: [String]) -> ConvertedInput {
-        input
+        var stones: ConvertedInput = [:]
+        input[0].components(separatedBy: " ").map{ Int($0)! }.forEach { stone in
+            stones[stone] = 1 + (stones[stone] ?? 0)
+        }
+        return stones
     }
-
+    
     static func solvePart1(input: ConvertedInput) -> String {
-        return "Add some Code here"
+        "\(blinking(25, with: input))"
     }
 
     static func solvePart2(input: ConvertedInput) -> String {
-        return "Add some Code here"
+        "\(blinking(75, with: input))"
+    }
+    
+    static func blinking(_ blinks: Int, with stones: ConvertedInput) -> Int {
+        var stones = stones
+        for _ in 1...blinks {
+            var newStones: ConvertedInput = [:]
+            for (stone, count) in stones {
+                if stone == 0 {
+                    newStones[1] = count + (newStones[1] ?? 0)
+                } else if String(stone).count.isEven {
+                    let stoneStr = String(stone)
+                    let length = stoneStr.count
+                    let first = Int(stoneStr.dropLast(length/2))!
+                    let last = Int(stoneStr.dropFirst(length/2))!
+                    newStones[first] = count + (newStones[first] ?? 0)
+                    newStones[last] = count + (newStones[last] ?? 0)
+                } else {
+                    let newValue = stone*2024
+                    newStones[newValue] = count + (newStones[newValue] ?? 0)
+                }
+            }
+            stones = newStones
+        }
+        
+        return stones.values.reduce(0, +)
     }
 }
--- AdventOfCode/main.swift
+++ AdventOfCode/main.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 let startTime = CFAbsoluteTimeGetCurrent()
-Year_2024().day_10.solve()
+Year_2024().day_11.solve()
 let diffTime = CFAbsoluteTimeGetCurrent() - startTime
 print("Took \(diffTime) seconds")
 
