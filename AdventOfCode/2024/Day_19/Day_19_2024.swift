1fef4826a17282eea34300b6db6148e5d7bc9615
Marcus Gollnick <marcus.gollnick@datev.de>
2024-12-20 14:04:07 +0100

    Solve Day 19 in 2024

--- AdventOfCode/2024/Day_19/Day_19_2024.swift
+++ AdventOfCode/2024/Day_19/Day_19_2024.swift
@@ -10,17 +10,61 @@
 enum Day_19_2024: Solvable {
     static var day: Input.Day = .Day_19
     static var year: Input.Year = .Year_2024
-    typealias ConvertedInput = [String]
-
+    typealias ConvertedInput = (patterns: [String], designs: [String])
+    
     static func convert(input: [String]) -> ConvertedInput {
-        input
+        var lines = input
+        let towels = lines.removeFirst().components(separatedBy: ", ")
+        lines.removeFirst()
+        return (towels, lines)
+    }
+    
+    static func isPossible(design: String, patterns: [String]) -> Bool {
+        guard !design.isEmpty else { return true }
+        for pattern in patterns {
+            var design = design
+            if design.starts(with: pattern) {
+                design.removeFirst(pattern.count)
+                if isPossible(design: design, patterns: patterns) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+    
+    static func possibleCount(design: String, patterns: [String], cache: inout [String: Int]) -> Int {
+        guard !design.isEmpty else { return 1 }
+        
+        if let cached = cache[design] {
+            return cached
+        }
+        
+        var count = 0
+        for pattern in patterns {
+            var design = design
+            if design.starts(with: pattern) {
+                design.removeFirst(pattern.count)
+                count += possibleCount(design: design, patterns: patterns, cache: &cache)
+            }
+        }
+        
+        cache[design] = count
+        
+        return count
     }
 
     static func solvePart1(input: ConvertedInput) -> String {
-        return "Add some Code here"
+        let possibleDesigns = input.designs.filter { isPossible(design: $0, patterns: input.patterns) }
+        return "\(possibleDesigns.count)"
     }
 
     static func solvePart2(input: ConvertedInput) -> String {
-        return "Add some Code here"
+        var count = 0
+        var cache: [String: Int] = [:]
+        input.designs.forEach { design in
+            count += possibleCount(design: design, patterns: input.patterns, cache: &cache)
+        }
+        return "\(count)"
     }
 }
--- AdventOfCode/main.swift
+++ AdventOfCode/main.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 let startTime = CFAbsoluteTimeGetCurrent()
-Year_2024().day_15.solve()
+Year_2024().day_19.solve()
 let diffTime = CFAbsoluteTimeGetCurrent() - startTime
 print("Took \(diffTime) seconds")
 
